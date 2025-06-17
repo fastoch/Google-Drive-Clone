b4741a0992c9b9ddfa92914db463e60904c578ce
fastoch <fastoch@ik.me>
2025-06-17 17:27:14 +0200

    @2:35

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="center">
-      <h1 className="text-3xl text-brand font-bold">
+    <div className="flex-center h-screen">
+      <h1 className="h1">
         StoreIt - The only storage solution you need.
       </h1>
     </div>
