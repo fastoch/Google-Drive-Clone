01be94670b3f455fda053652436dfe1ab359e45d
fastoch <fastoch@ik.me>
2025-06-17 19:34:03 +0200

    @2:41

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Image from 'next/image';
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='flex min-h-screen'>
+      <section className='bg-brand p-10'>
+        <div>
+          <Image src='/assets/icons/logo-full.svg' alt='logo' width={224} height={82} className="h-auto" />
+          <div className='space-y-5 text-white'>
+            <h1 className='h1'>Manage your files the best way</h1>
+            <p className="body-1">
+              This is a place where you can store all your documents and access them from anywhere.
+            </p>
+          </div>
+          <Image src="/assets/images/files.png" alt="Files" width={342} height={342} className='transition-all hover:rotate-2 hover:scale-105' />
+        </div>
+      </section>
+      {children}
+    </div>
+  );
+}
+
+export default Layout
